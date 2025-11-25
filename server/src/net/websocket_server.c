ed930ae88ae39de29b8c85087aa7423fd721376f
kreid06 <kevinreid3light@gmail.com>
2025-11-24 21:32:21 -0500

    fixed helm local to world coords

--- server/src/net/websocket_server.c
+++ server/src/net/websocket_server.c
@@ -1684,8 +1684,8 @@ int websocket_server_update(struct Sim* sim) {
                                             // Add modules
                                             for (int m = 0; m < ships[s].module_count && ships_offset < (int)sizeof(ships_str) - 200; m++) {
                                                 const ShipModule* module = &ships[s].modules[m];
-                                                float module_x = Q16_TO_FLOAT(module->local_pos.x);
-                                                float module_y = Q16_TO_FLOAT(module->local_pos.y);
+                                                float module_x = SERVER_TO_CLIENT(Q16_TO_FLOAT(module->local_pos.x));
+                                                float module_y = SERVER_TO_CLIENT(Q16_TO_FLOAT(module->local_pos.y));
                                                 float module_rot = Q16_TO_FLOAT(module->local_rot);
                                                 
                                                 ships_offset += snprintf(ships_str + ships_offset, sizeof(ships_str) - ships_offset,
@@ -2246,8 +2246,8 @@ int websocket_server_update(struct Sim* sim) {
                     // Add modules from simple ships
                     for (int m = 0; m < ships[s].module_count && offset < (int)sizeof(ship_entry) - 200; m++) {
                         const ShipModule* module = &ships[s].modules[m];
-                        float module_x = Q16_TO_FLOAT(module->local_pos.x);
-                        float module_y = Q16_TO_FLOAT(module->local_pos.y);
+                        float module_x = SERVER_TO_CLIENT(Q16_TO_FLOAT(module->local_pos.x));
+                        float module_y = SERVER_TO_CLIENT(Q16_TO_FLOAT(module->local_pos.y));
                         float module_rot = Q16_TO_FLOAT(module->local_rot);
                         
                         offset += snprintf(ship_entry + offset, sizeof(ship_entry) - offset,
